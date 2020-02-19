3be7d48de382515c5d3d6b408589a2a231fb1db6
Abdoul Guisset <guissetabdoul@gmail.com>
2020-02-19 15:40:06 -0500

    Dice Project tutorial

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    // IBoutlet allows me reference an UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!;
+    
+    
+    //code that will be trigger when a particular action happens
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]; // let is for constant variable
+        
+        diceImageView1.image = diceArray.randomElement(); // same as diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]; // same as diceImageView2
+        
     }
-
-
+    
 }
 
