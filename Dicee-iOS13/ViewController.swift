397ebadd138a569c329e6681e798453362f37f21
Abdoul Guisset <guissetabdoul@gmail.com>
2020-03-03 22:05:22 -0500

    Adding AutoLayout

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,26 +1,24 @@
 //
 //  ViewController.swift
-//  Dicee-iOS13
+//  AutoLayout-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    // IBoutlet allows me reference an UI element
     @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!;
+    @IBOutlet weak var diceImageView2: UIImageView!
     
-    
-    //code that will be trigger when a particular action happens
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]; // let is for constant variable
         
-        diceImageView1.image = diceArray.randomElement(); // same as diceArray[Int.random(in: 0...5)]
-        diceImageView2.image = diceArray[Int.random(in: 0...5)]; // same as diceImageView2
+        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = allDice[Int.random(in: 0...5)]
+        diceImageView2.image = allDice[Int.random(in: 0...5)]
         
     }
     
